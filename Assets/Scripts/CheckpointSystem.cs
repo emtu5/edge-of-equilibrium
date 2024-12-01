66868696cdee0efd9acf9cebd208f2503c2a5e20
emtu5 <115076076+emtu5@users.noreply.github.com>
2024-12-01 19:14:39 +0200

    reset player momentum on respawn

--- Assets/Scripts/CheckpointSystem.cs
+++ Assets/Scripts/CheckpointSystem.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class CheckpointSystem : MonoBehaviour
 {
-    private Vector3 respawnPosition; 
+    private Vector3 respawnPosition;
+    private Rigidbody rigidbody;
     public Transform player; 
     void Start()
     {
         respawnPosition = player.position;
+        rigidbody = player.GetComponent<Rigidbody>();
     }
 
     void OnTriggerEnter(Collider other)
@@ -21,5 +23,10 @@ public class CheckpointSystem : MonoBehaviour
     public void RespawnPlayer()
     {
         player.position = respawnPosition;
+
+        // reset player momentum
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        rigidbody.rotation = Quaternion.identity;
     }
 }
